0bd0499f147f8a4723824c4c19684416af3bbc0f
Djamel F <djamelfel@gmail.com>
2015-01-13 10:55:18 +0100

    type_output is mandatory by dispatcher.lua

--- toml/filters/dispatcher.lua
+++ toml/filters/dispatcher.lua
@@ -5,33 +5,33 @@ local list_item = { }
 
 function init_list()
     for value in string.gmatch(list, "[%S]+") do
-	local sandbox = read_config(value .. '_type_output')
 	local regex   = read_config(value .. '_regex')
-
-        if sandbox and regex then
-            list_item[#list_item + 1] = {
-		sandbox = sandbox,
-	        regex   = regex
-	    }
+	local type_output= read_config(value .. '_type_output')
+	if type_output == nil or regex == nil then
+	    return 1
 	end
+
+	list_item[#list_item + 1] = {
+	    type_output = type_output,
+	    regex   = regex
+	}
     end
 end
 init_list()
 
 function process_message()
     local name = read_message('Fields[name]')
-    local next_sandbox = nil
+    local type_output = nil
 
     for index, value in ipairs(list_item) do
 	if string.find(name, "^" .. value.regex .. "$") ~= nil then
-	    next_sandbox = value.sandbox
+	    type_output = value.type_output
 	    break
 	end
     end
 
-    if not next_sandbox then return -1 end
     local data = {
-	Type    = next_sandbox,
+	Type    = type_output,
 	Fields  = {
 	    name  = name,
 	    value = read_message('Fields[value]')
