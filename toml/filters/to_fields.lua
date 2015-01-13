8e2c9339ec6619c957c7b6466eb404fc3325f803
Djamel F <djamelfel@gmail.com>
2015-01-13 16:31:33 +0100

    configure heka with a gust for wind metrics

--- toml/filters/to_fields.lua
+++ toml/filters/to_fields.lua
@@ -10,10 +10,6 @@ for value in string.gmatch(matchers, "[%S]+") do
 end
 
 function process_message()
-    if read_config('emit_in_payload') then
-	data.Payload = read_message('Payload')
-    end
-
     data.Type = read_config('type_output')
     if data.Type == nil then
 	return 1
@@ -22,6 +18,9 @@ function process_message()
     data.Fields.name = read_message('Fields[name]')
     data.Fields.value = read_message('Fields[value]')
 
+    if read_config('emit_in_payload') then
+	data.Payload = data.Fields.name .. ':' .. data.Fields.value
+    end
     inject_message(data)
 
     return 0
