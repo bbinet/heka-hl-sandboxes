e3ce53fbe2f7e6def311bf27670d1564507c9500
Djamel F <djamelfel@gmail.com>
2015-01-14 19:14:47 +0100

    correct issue from trserver.lua

--- toml/decoders/trserver.lua
+++ toml/decoders/trserver.lua
@@ -1,24 +1,20 @@
 require "string"
+local type_output = read_config('type_output')
+if type_output == nil then
+    return 1
+end
 
 function process_message()
-    local type_output = read_config('type_output')
-
-    if type_output == nil then
-        return 1
-    end
-
     local data = {
         Type    = type_output,
         Fields  = { }
     }
 
-    local payload = read_message("Payload") .. ''
-    data.Fields.name, data.Fields.value = string.match(payload, "^([%w_]+):([%w_.+-]+)|p$")
+    data.Fields.name, data.Fields.value = string.match(read_message('Payload'), "^([%w_]+):([%w_.+-]+)|p$")
 
     data.Payload = read_message('Timestamp') .. ':' .. data.Fields.name .. ':' .. data.Fields.value
 
     inject_message(data)
-    data = { }
 
     return 0
 end
