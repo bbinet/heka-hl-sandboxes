cc20b8b24770a9eab47f448b723784f0f2aacd18
Djamel F <djamelfel@gmail.com>
2015-01-15 11:14:57 +0100

    improve group metrics sandbox

--- toml/filters/group_metrics.lua
+++ toml/filters/group_metrics.lua
@@ -1,19 +1,18 @@
-local type_output = read_config('type_output')
-if type_output == nil then
-    return 1
-end
-local data = {
-    Type = type_output,
-    Timestamp = read_message('Timestamp'),
-    Fields = { }
+local type_output = read_config('type_output') or error('you must initialize "type_output" option')
+local fields = {
 }
 
 function process_message()
-    data.Fields[read_message('Fields[name]')] = read_message('Fields[value]')
+    fields[read_message('Fields[name]')] = read_message('Fields[value]')
 
     return 0
 end
 
 function timer_event(ns)
-    inject_message(data)
+    inject_message({
+	Type = type_output,
+	Timestamp = read_message('Timestamp'),
+	Fields = fields
+    })
+    fields = { }
 end
