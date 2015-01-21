5ba78f177187b8c077cbf79b8c5692f090a9f22f
Djamel F <djamelfel@gmail.com>
2015-01-21 18:36:17 +0100

    remove data variable

--- toml/decoders/trserver_decode_metrics.lua
+++ toml/decoders/trserver_decode_metrics.lua
@@ -2,12 +2,7 @@ require "string"
 local type_output = read_config('type_output') or error('you must initialize "type_output" option')
 
 function process_message()
-    local data = {
-        Type = type_output,
-        Payload = read_message('Payload'),
-        Fields = { }
-    }
-
+    local payload = read_message('Payload')
     local name, value = string.match(data.Payload, "^([%w_]+):([%w_.+-]+)|p$")
     value = tonumber(value)
 
@@ -15,7 +10,14 @@ function process_message()
         data.Fields.name = name
         data.Fields.value = value
 
-        inject_message(data)
+        inject_message({
+            Type = type_output,
+            Payload = payload,
+            Fields = {
+                name = name,
+                value = value
+            }
+        })
     end
 
     return 0
