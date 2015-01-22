e31776956cfb19e617e01aa65e833ec417c12966
Djamel F <djamelfel@gmail.com>
2015-01-22 09:42:23 +0100

    improve code source from regex_dispatch_metric sandbox

--- toml/filters/regex_dispatch_metric.lua
+++ toml/filters/regex_dispatch_metric.lua
@@ -4,36 +4,29 @@ local matcher = read_config('matchers') or error ('you must initialize "matchers
 local matchers = { }
 
 for value in string.gmatch(matcher, "[%S]+") do
-    local regex = read_config(value .. '_regex') or error ('you must initialize "' .. value .. '_regex" option')
-    local type_output = read_config(value .. '_type_output') or error ('you must initialize "' .. value .. '_type_output" option')
-
     matchers[#matchers + 1] = {
-	type_output = type_output,
-	regex = regex
+	type_output = read_config(value .. '_type_output') or error ('you must initialize "' .. value .. '_type_output" option'),
+	regex = read_config(value .. '_regex') or error ('you must initialize "' .. value .. '_regex" option')
     }
 end
 
 function process_message()
     local name = read_message('Fields[name]')
-    local type_output = nil
 
     for index, item in ipairs(matchers) do
 	if string.find(name, "^" .. item.regex .. "$") ~= nil then
-	    type_output = item.type_output
+	    inject_message({
+		Type = item.type_output,
+		Timestamp = read_message('Timestamp'),
+		Payload = read_message('Payload'),
+		Fields = {
+		    name = name,
+		    value = read_message('Fields[value]')
+		}
+	    })
 	    break
 	end
     end
 
-    if type_output ~= nil then
-	inject_message({
-	    Type = type_output,
-	    Timestamp = read_message('Timestamp'),
-	    Payload = read_message('Payload'),
-	    Fields = {
-		name = name,
-		value = read_message('Fields[value]')
-	    }
-	})
-    end
     return 0
 end
