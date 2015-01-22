1fecc26bf6a8bbcd3379de6817b2b854178ae051
Djamel F <djamelfel@gmail.com>
2015-01-22 17:39:11 +0100

    use property of table.concat

--- toml/encoders/encode_header.lua
+++ toml/encoders/encode_header.lua
@@ -1,12 +1,14 @@
 require "string"
+require "table"
 require "os"
 
 function process_message()
-    local data = os.date("%y%m%d %X", read_message('Timestamp')/1e9)
-    data = data .. ' ' .. read_message('Fields[uuid]')
-    data = data .. ' ' .. read_message('Fields[hostname]')
-    data = data .. ' ' .. read_message('Fields[type]')
-    data = data .. ' ' .. 0 ..':' .. read_message('Fields[version]')
+    local data = { }
+    data[#data+1] = os.date("%y%m%d %X", read_message('Timestamp')/1e9)
+    data[#data+1] = read_message('Fields[uuid]')
+    data[#data+1] = read_message('Fields[hostname]')
+    data[#data+1] = read_message('Fields[type]') .. ':' .. 0 .. ':' .. read_message('Fields[version]')
+    data = table.concat(data, " ")
     data = '[' .. data .. ']' .. read_message('Fields[log]') .. '\n'
 
     inject_payload('txt', 'log_parse', string.format(data))
