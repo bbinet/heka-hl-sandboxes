5b5ddd10459307cb2866b906f1dd0b8c9d87e795
Djamel F <djamelfel@gmail.com>
2015-01-22 17:58:04 +0100

    keep time in epoch format with all this precision

--- toml/encoders/encode_header.lua
+++ toml/encoders/encode_header.lua
@@ -4,7 +4,8 @@ require "os"
 
 function process_message()
     local data = { }
-    data[#data+1] = os.date("%y%m%d %X", read_message('Timestamp')/1e9)
+    data[#data+1] = os.date("%X", read_message('Timestamp')/1e9)
+    data[#data+1] = read_message('Timestamp')
     data[#data+1] = read_message('Fields[uuid]')
     data[#data+1] = read_message('Fields[hostname]')
     data[#data+1] = read_message('Fields[type]') .. ':' .. 0 .. ':' .. read_message('Fields[version]')
