a3a9804f90404a71a57d956316f83eda7d836a50
Djamel F <djamelfel@gmail.com>
2015-01-26 16:18:57 +0100

    decode event log message

---
+++ toml/filters/decode_event.lua
@@ -0,0 +1,27 @@
+require "string"
+
+local type_output = read_config('type_output') or error('you must initialize "type_output" option')
+
+function process_message()
+    local payload = read_message('Payload')
+    local message = string.match(payload, '^"(.+)"$')
+    local fields = {
+	message = message
+    }
+
+    while true do
+	typ, name, value = read_next_field()
+	if not typ then break end
+	if typ ~= 1 then --exclude bytes
+	    fields[name] = value
+	end
+    end
+
+    inject_message({
+	Type = type_output,
+	Timestamp = read_message('Timestamp'),
+	Payload = payload,
+	Fields = fields
+    })
+    return 0
+end
