a6d34fa4aa12778d963f3c4db6bd6702e5a5b423
Djamel F <djamelfel@gmail.com>
2015-01-27 16:23:02 +0100

    test value from data if value is nul the sandbox will be done
    to improve when the api method will be available

--- toml/encoders/encode_header.lua
+++ toml/encoders/encode_header.lua
@@ -6,9 +6,15 @@ function process_message()
     local data = { }
     data[#data+1] = os.date("%X", read_message('Timestamp')/1e9)
     data[#data+1] = read_message('Timestamp')
-    data[#data+1] = read_message('Fields[uuid]')
-    data[#data+1] = read_message('Fields[hostname]')
-    data[#data+1] = read_message('Fields[type]') .. ':' .. read_message('Fields[encoder_version]')
+    data[#data+1] = read_message('Fields[uuid]') --or TODO: print error message
+    data[#data+1] = read_message('Fields[hostname]') --or TODO: print error message
+    data[#data+1] = read_message('Fields[type]') .. ':' .. read_message('Fields[encoder_version]') --or TODO: print error message
+
+    for k, v in pairs(data) do
+        if v == nil then
+            return 0
+        end
+    end
 
     inject_payload('txt', 'log_parse', '[' .. table.concat(data, " ") .. '] ' .. read_message('Payload') .. '\n')
 
