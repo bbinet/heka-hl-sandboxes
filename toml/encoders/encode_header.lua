01e74f141461cac4644f09d1a4523356be65fbb3
Djamel F <djamelfel@gmail.com>
2015-01-28 14:32:54 +0100

    cosmetic change, inverse timestamp, uuid, hostname will be hostname, uuid, timestamp

--- toml/encoders/encode_header.lua
+++ toml/encoders/encode_header.lua
@@ -5,9 +5,9 @@ require "os"
 function process_message()
     local data = { }
     data[#data+1] = os.date("%X", read_message('Timestamp')/1e9)
-    data[#data+1] = string.format("%d", read_message('Timestamp'))
-    data[#data+1] = read_message('Fields[uuid]')
     data[#data+1] = read_message('Fields[hostname]')
+    data[#data+1] = read_message('Fields[uuid]')
+    data[#data+1] = string.format("%d", read_message('Timestamp'))
     data[#data+1] = read_message('Fields[type]') .. ':' .. read_message('Fields[encoder_version]')
 
     for k, v in pairs(data) do --TODO: print error message
