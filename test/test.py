17a415677bb3705c398dd820a420dc50190036ab
Djamel F <djamelfel@gmail.com>
2015-02-05 16:57:03 +0100

    use Upd connection instead of Tcp connection

--- test/test.py
+++ test/test.py
@@ -7,62 +7,71 @@ import json
 import tempfile
 import shutil
 
-TCP_IP = 'localhost'
-TCP_PORT = 5005
+HEKA_IP = 'localhost'
+HEKA_INPUT_PORT = 5007
+HEKA_OUTPUT_PORT = 5005
 
 def setUpModule():
 	global PROC
-	global TEMP_DIR
-	global SOCKET_S
-	global SOCKET_R
-	global RECEIPT
-
-	TEMP_DIR = tempfile.mkdtemp()
 	PROC = subprocess.Popen(['hekad', '-config', 'heka.toml'], stderr=subprocess.PIPE)
 	time.sleep(1)
-	SOCKET_S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	SOCKET_S.connect((TCP_IP, TCP_PORT))
-	SOCKET_R = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	SOCKET_R.bind((TCP_IP, 5007))
-	SOCKET_R.listen(1)
-	RECEIPT, _ = SOCKET_R.accept()
 
 def tearDownModule():
 	global PROC
-	global TEMP_DIR
-	global SOCKET_S
-	global RECEIPT
-
-	SOCKET_S.close()
-	RECEIPT.close()
 	PROC.terminate()
-	shutil.rmtree(TEMP_DIR)
 
-class TestAddFields(unittest.TestCase):
+class HekaTestCase(unittest.TestCase):
 	def setUp(self):
-		global TEMP_DIR
-		with open(TEMP_DIR + '/add_fields.toml', 'w') as f:
-			f.write("""
-				[AddFieldsFilter]
-				type = "SandboxFilter"
-				message_matcher = "Type == 'add.fields'"
-				[AddFieldsFilter.config]
-				fields = "uuid"
-				uuid = "uuid_test"
-				type_output = "output"
-			""")
+		self.tmpdir = tempfile.mkdtemp()
+		self.tmpconfig = os.path.join(self.tmpdir, 'config.toml')
+		with open(self.tmpconfig, 'w') as f:
+			f.write(self.config)
 			f.flush()
-		subprocess.check_call(['heka-sbmgr', '-action=load', '-config=PlatformTest.toml', '-script=' + os.path.abspath('..') + '/filters/add_static_fields.lua', '-scriptconfig=' + TEMP_DIR + '/add_fields.toml'])
+		subprocess.check_call([
+			'heka-sbmgr',
+			'-action=load',
+			'-config=PlatformTest.toml',
+			'-script=' + os.path.abspath(self.sandbox),
+			'-scriptconfig=' + self.tmpconfig])
+		time.sleep(1)
+		self.heka_output = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.heka_input = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.heka_input.bind((HEKA_IP, HEKA_INPUT_PORT))
 
 	def tearDown(self):
-		subprocess.check_call(['heka-sbmgr', '-action=unload', '-config=PlatformTest.toml', '-filtername=AddFieldsFilter'])
+		for u in self.unload:
+			subprocess.check_call([
+			'heka-sbmgr',
+			'-action=unload',
+			'-config=PlatformTest.toml',
+			'-filtername=' + u])
+		shutil.rmtree(self.tmpdir)
+
+class TestAddFields(HekaTestCase):
+	sandbox = '../filters/add_static_fields.lua'
+	config = """
+[AddFieldsFilter]
+type = "SandboxFilter"
+message_matcher = "Type == 'add.fields'"
+[AddFieldsFilter.config]
+fields = "uuid"
+uuid = "uuid_test"
+type_output = "output"
+"""
+	unload = ['AddFieldsFilter']
 
 	def test_sandbox(self):
-		global SOCKET_S
-		global RECEIPT
-
-		SOCKET_S.send(json.dumps({'Timestamp': 10, 'Type': 'add.fields', 'Payload': 'titi', 'Fields': {'name': 'tata', 'value': 'toto'}})+'\n')
-		data = json.loads(RECEIPT.recv(5000))
+		self.heka_output.sendto(json.dumps({
+			'Timestamp': 10,
+			'Type': 'add.fields',
+			'Payload': 'titi',
+			'Fields': {
+				'name': 'tata',
+				'value': 'toto'
+				}
+			})+'\n', (HEKA_IP, HEKA_OUTPUT_PORT))
+		data, _ = self.heka_input.recvfrom(5000)
+		data = json.loads(data)
 		self.assertEqual(data['Fields']['uuid'], 'uuid_test')
 
 if __name__ == '__main__':
