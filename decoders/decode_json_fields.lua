08b140c45ae46f8989139cd36f3811d64641450d
Bruno Binet <bruno.binet@gmail.com>
2015-03-17 15:29:49 +0100

    refactor trwebclient_decode_event.lua to decode_json_fields.lua
    make it more generic

--- decoders/decode_json_fields.lua
+++ decoders/decode_json_fields.lua
@@ -3,16 +3,14 @@ require "cjson"
 local type_output = read_config('type_output') or error('you must initialize "type_output" option')
 
 function process_message()
-    local payload = cjson.decode(read_message('Payload'))
+    local fields = cjson.decode(read_message('Payload'))
+    if type(fields) ~= "table" then
+        return -1, "invalid json"
+    end
 
     inject_message({
         Type = type_output,
-        Timestamp = timestamp,
-        Payload = log,
-        Fields = {
-            log = payload.log,
-            msg = payload.msg
-        }
+        Fields = fields
     })
 
     return 0
