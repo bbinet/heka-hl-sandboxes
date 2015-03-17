13037cd81a7cfc4667f7b26c8fa2a5a682c8a914
Bruno Binet <bruno.binet@gmail.com>
2015-03-17 11:43:52 +0100

    don't die when severity is missing

--- filters/encode_event.lua
+++ filters/encode_event.lua
@@ -5,6 +5,16 @@ local version = 0
 local type_output = read_config('type_output') or error('you must initialize "type_output" option')
 
 function process_message()
+    -- check that both Severity and Fields[msg] exist
+    local severity = read_message('Severity')
+    local msg = read_message('Fields[msg]')
+    if severity == nil then
+	return -1, "severity can't be nil"
+    end
+    if msg == nil then
+	return -1, "msg can't be nil"
+    end
+    
     local fields = {
 	encoder_type = "event",
 	encoder_version = version
@@ -20,7 +30,7 @@ function process_message()
 
     inject_message({
 	Type = type_output,
-	Payload = read_message('Severity') .. ' ' .. cjson.encode(read_message('Fields[msg]')),
+	Payload = severity .. ' ' .. cjson.encode(msg),
 	Timestamp = read_message('Timestamp'),
 	Fields = fields
     })
