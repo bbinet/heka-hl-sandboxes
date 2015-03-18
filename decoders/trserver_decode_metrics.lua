958121970460a82f88b548cfd563d3e90b5d201b
Bruno Binet <bruno.binet@gmail.com>
2015-03-18 16:22:33 +0100

    validate "name" and "value" in trserver decoded metrics

--- decoders/trserver_decode_metrics.lua
+++ decoders/trserver_decode_metrics.lua
@@ -2,14 +2,18 @@ require "string"
 local type_output = read_config('type_output') or error('you must initialize "type_output" option')
 
 function process_message()
-    local payload = read_message('Payload')
-    local name, value = string.match(payload, "^([%w_]+):([%w_.+-]+)|p\n$")
+    local name, value = string.match(read_message('Payload'), "^([%w_]+):([%w_.+-]+)|p\n$")
     value = tonumber(value)
+    if name == nil then
+	return -1, "name can't be nil"
+    end
+    if value == nil then
+	return -1, "value can't be nil"
+    end
 
     if name ~= nil and value ~= nil then
         inject_message({
             Type = type_output,
-            Payload = payload,
             Fields = {
                 name = name,
                 value = value
