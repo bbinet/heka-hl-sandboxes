8f48928ee579c09c4c7fbfe58edf5a2c408a3fbe
Bruno Binet <bruno.binet@gmail.com>
2015-04-30 18:35:15 +0200

    update carbon encoder following aggregate_metric changes
    aggregated metric heka messages now contain multiple metrics in the same
    messages

--- encoders/encode_carbon.lua
+++ encoders/encode_carbon.lua
@@ -1,26 +1,30 @@
 require "string"
 require "table"
 
+local uuid = read_config('uuid') or error ('you must initialize "uuid" option')
+local hostname = read_config('hostname') or error ('you must initialize "hostname" option')
+
 function process_message()
     local timestamp = read_message('Timestamp')
-    local name = read_message('Fields[name]')
-    local value = read_message('Fields[value]')
-
     if timestamp == nil then
         return -1, "Timestamp can't be nil"
     end
-    if name == nil then
-        return -1, "Fields[name] can't be nil"
-    end
-    if value == nil then
-        return -1, "Fields[value] can't be nil"
+    local timestr = string.format("%d", timestamp/1e9)
+
+    while true do
+	typ, key, value = read_next_field()
+	if not typ then break end
+        -- exclude bytes and internal fields starting with '_' char
+	if typ ~= 1 and key:match'^(.)' ~= '_' then
+            add_to_payload(table.concat({
+                table.concat({uuid, hostname, key}, '.'),
+                value,
+                timestr
+            }, " ") .. '\n')
+	end
     end
 
-    inject_payload('txt', 'encode_carbon', table.concat({
-        name,
-        value,
-        string.format("%d", timestamp/1e9),
-    }, " ") .. '\n')
+    inject_payload('txt', 'encode_carbon')
 
     return 0
 end
