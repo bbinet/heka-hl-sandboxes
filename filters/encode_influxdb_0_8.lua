22482ce989686a19bc35e3a444e11cf403c63eeb
Bruno Binet <bruno.binet@gmail.com>
2015-05-06 17:14:58 +0200

    update filters/encode_influxdb_0_8 encoder following aggregate_metric changes
    aggregated metric heka messages now contain multiple metrics in the same
    messages

--- filters/encode_influxdb_0_8.lua
+++ filters/encode_influxdb_0_8.lua
@@ -1,16 +1,29 @@
 require "cjson"
+require "string"
 
 local type_output = read_config('type_output') or error('you must initialize "type_output" option')
 local metrics = {}
 
 function process_message()
-    local metric = {
-	name = read_message("Fields[name]"),
-	columns = {'time', 'value'},
-	--TODO: remove the division with the release from influxDB 0.9
-	points = {{read_message("Timestamp")/1e3, read_message("Fields[value]")}}
-    }
-    metrics[#metrics+1] = metric
+    local timestamp = read_message('Timestamp')
+    if timestamp == nil then
+        return -1, "Timestamp can't be nil"
+    end
+    -- TODO: remove the division with the release from influxDB 0.9
+    local ts = timestamp/1e3
+
+    while true do
+	typ, key, value = read_next_field()
+	if not typ then break end
+        -- exclude bytes and internal fields starting with '_' char
+	if typ ~= 1 and key:match'^(.)' ~= '_' then
+	    metrics[#metrics+1] = {
+		name = key,
+		columns = {'time', 'value'},
+		points = {{ts, value}}
+	    }
+	end
+    end
 
     return 0
 end
